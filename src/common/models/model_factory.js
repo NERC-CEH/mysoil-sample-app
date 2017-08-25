69e0680b2fe5055acad83c946ae23414af76239d
Jim Bacon <jame2@ceh.ac.uk>
2017-08-25 14:10:53 +0100

    Pad uid with leading zeros
    Improve uniqueness and allow data and time to be easily inferred from uid.

--- src/common/models/model_factory.js
+++ src/common/models/model_factory.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import ImageHelp from 'helpers/image';
 import userModel from 'user_model';
 import appModel from 'app_model';
@@ -7,10 +8,15 @@ import Occurrence from './occurrence';
 
 const Factory = {
   createSample() {
+    // Generate a uid of form uid-yyyymmdd-hhmmss
     let uid = userModel.get('drupalID');
     let now = new Date();
-    uid += '-' + now.getFullYear() + (now.getMonth() + 1) + now.getDate();
-    uid += '-' + now.getHours() + now.getMinutes() + now.getSeconds();
+    uid += '-' + now.getFullYear();
+    uid += _.padLeft(now.getMonth() + 1, 2, '0');
+    uid += _.padLeft(now.getDate(), 2, '0');
+    uid += '-' + _.padLeft(now.getHours(), 2, '0');
+    uid += _.padLeft(now.getMinutes(), 2, '0');
+    uid += _.padLeft(now.getSeconds(), 2, '0');
    
     const sample = new Sample({
       uid: uid,
