98ad2196eccfa24621c9c3cc1fd309f9a0c1cc4a
Jim Bacon <jame2@ceh.ac.uk>
2017-09-07 12:20:42 +0100

    Redirect to https
    Force use of https when serving as a web app to ensure location services work.

--- config/server.js
+++ config/server.js
@@ -11,6 +11,19 @@ const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+  // When using Express and Node to serve mySoil Sample as a web app it must be
+  // done over https for location services to work. Force a redirect if http is used. 
+  // Suitable for Heroku. May need adjusting for other platforms.
+  if (req.headers['x-forwarded-proto'] !== 'https' &&
+      req.hostname !== 'localhost') {
+    res.redirect('https://' + req.hostname + req.url) 
+  }
+  else {
+    next();
+  }
+});
+
 app.use(express.static('dist/main'));
 
 const server = app.listen(PORT, function () {
